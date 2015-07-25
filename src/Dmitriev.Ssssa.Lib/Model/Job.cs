16ae3a43e07572d3039dd50afe79f461778ca11a
Denis Dmitriev <denis.dmitriev@gmail.com>
2015-07-25 11:45:22 +0300

    Rename some namespaces and add basic model classes

---
+++ src/Dmitriev.Ssssa.Lib/Model/Job.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dmitriev.Ssssa.Model
+{
+  public class Job
+  {
+    public Guid          Id    { get; set; }
+    public List<JobStep> Steps { get; set; }
+  }
+}
---
+++ src/Dmitriev.Ssssa.Lib/Model/JobStep.cs
@@ -0,0 +1,8 @@
+﻿namespace Dmitriev.Ssssa.Model
+{
+  public class JobStep
+  {
+    public int    Id      { get; set; }
+    public string SqlCode { get; set; } 
+  }
+}
